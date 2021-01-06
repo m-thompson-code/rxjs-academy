17f9bd7968a36d3079c3ea4831e99ab033a33382
Mark Thompson <mark.thompson@smpl.company>
2021-01-06 12:17:49 -0800

    RxJS in Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { BehaviorSubject, Observable, Subscription } from 'rxjs';
 
 @Component({
-  selector: 'moo-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'moo-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  
+export class AppComponent implements OnInit {
+    public userCardNumber = new BehaviorSubject<string>('');
+    private _sub!: Subscription;
+
+    public ngOnInit(): void {
+        this._sub = new Subscription();
+    }
+
+    public handleFakeNumberInput(event: Event): void {
+        const _target = event.target as HTMLInputElement;
+        this.userCardNumber.next( _target.value || '' );
+    }
+
+    public ngOnDestroy(): void {
+        this._sub.unsubscribe();
+    }
 }
