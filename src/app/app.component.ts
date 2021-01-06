9d06b79b61b93d2ab428e6532b5aec1ff48684aa
Mark Thompson <mark.thompson@smpl.company>
2021-01-06 12:36:43 -0800

    Clean card number

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { BehaviorSubject, Observable, Subscription } from 'rxjs';
+
+import { BehaviorSubject, Observable, Subject, Subscription } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+const getCleanCardNumber = (cardNumber: string): string => {
+    return (cardNumber || '').replace(/[\s-]/g, "");
+}
 
 @Component({
     selector: 'moo-root',
@@ -7,11 +13,18 @@ import { BehaviorSubject, Observable, Subscription } from 'rxjs';
     styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-    public userCardNumber = new BehaviorSubject<string>('');
+    public userCardNumber!: BehaviorSubject<string>;
+    public cardNumber!: Observable<string>;
     private _sub!: Subscription;
 
     public ngOnInit(): void {
         this._sub = new Subscription();
+
+        this.userCardNumber = new BehaviorSubject<string>('');
+
+        const mapToCleanCardNumber = map(getCleanCardNumber);
+
+        this.cardNumber = this.userCardNumber.pipe(mapToCleanCardNumber);
     }
 
     public handleFakeNumberInput(event: Event): void {
